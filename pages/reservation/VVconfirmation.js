d76c1a73914bb902faea6833aa928dbd80f30ad6
DorianMathieu <dorian.mathieu@cpe.fr>
2021-06-08 21:47:18 +0200

    Vreview-plein-truc-marche

--- pages/reservation/VVconfirmation.js
+++ pages/reservation/VVconfirmation.js
@@ -1,3 +1,5 @@
+import * as VVvars from '../../shared/VVvar.js';
+
 window.addEventListener("load", init);
 
 function $_GET(fieldN) {
@@ -15,13 +17,20 @@ function $_GET(fieldN) {
 	return VVvars;
 }
 
+var id
 function init()
 {
+    id = $_GET('id_rbot')
+    var D_data = VVvars.getById(id)
+
+    let template = document.querySelector("#info_recap");
+
+
     var fname=$_GET("fname")
     console.log(fname)
     var lname=$_GET("lname")
     console.log(lname)
-    var amail=$_GET("amail")
+    var amail=$_GET("amail").replace('%40','@')
     var tel=$_GET("tel")
     var pbtype=$_GET("pbtype")
     var start_d=$_GET("start_d")
@@ -30,5 +39,20 @@ function init()
     var nb_children=$_GET("nb_children")
     var breakfast=$_GET("breakfast")
     var infos_s=$_GET("infos_s")
-    var prix=$_GET("prix")
+    var price=$_GET("price")
+
+    let clone = document.importNode(template.content, true);      // clone le template
+    var newContent = clone.firstElementChild.innerHTML		// remplace {{modèle}}
+      .replace(/{{id}}/g, D_data.id)
+      //.replace(/{{nom}}/g, nom)
+      .replace(/{{price}}/g, price)
+      .replace(/{{imgPath}}/g, D_data.imgPath)
+      .replace(/{{fname}}/g, fname)
+      .replace(/{{lname}}/g, lname)
+      .replace(/{{amail}}/g, amail)
+      .replace(/{{nb_ad}}/g, nb_ad)
+      .replace(/{{nb_children}}/g, nb_children);
+
+  clone.firstElementChild.innerHTML = newContent	
+  document.getElementById('template_confirm').appendChild(clone);	console.log(D_data.imgPath);
 }
--- pages/reservation/VVresa.js
+++ pages/reservation/VVresa.js
@@ -1,21 +1,67 @@
-import * as VVvar from '../../shared/VVvar.js';
-var id;
+import * as VVvars from '../../shared/VVvar.js';
+
 
 window.addEventListener("load", init);
 
+var id
 
 function init(){
+    id = $_GET('id')
+    var D_data = VVvars.getById(id)
 
+    let template = document.querySelector("#info_destination");
+
+    let clone = document.importNode(template.content, true);      // clone le template
+  var newContent = clone.firstElementChild.innerHTML		// remplace {{modèle}}
+      .replace(/{{id}}/g, D_data.id)
+      .replace(/{{nom}}/g, D_data.nom)
+      .replace(/{{price}}/g, D_data.price)
+      .replace(/{{imgPath}}/g, D_data.imgPath)
+      .replace(/{{alt}}/g, D_data.alt)
+      .replace(/{{animaux}}/g, D_data.animaux)
+      .replace(/{{petitDej}}/g, D_data.dej);
+
+  clone.firstElementChild.innerHTML = newContent	
+  document.getElementById('template_resa').appendChild(clone);	console.log(D_data.imgPath);
+
+
+
+  let template_rbot = document.querySelector("#not_robot");
+
+  let clone_rbot = document.importNode(template_rbot.content, true);      // clone le template
+    var newContent = clone_rbot.firstElementChild.innerHTML		// remplace {{modèle}}
+    .replace(/{{id}}/g, D_data.id)
+
+    clone_rbot.firstElementChild.innerHTML = newContent	
+    document.getElementById('template_robot').appendChild(clone_rbot);	console.log(D_data.imgPath);
+
+  template_robot
 
   var inputs = document.getElementById('validation_formulaire').getElementsByTagName('input')
   for(var i = 0; i < inputs.length; i++) {
       inputs[i].addEventListener('change', verif) 
   }
+}
 
+function $_GET(fieldN) {
+	var VVvars = {};
+	window.location.href.replace( location.hash, '' ).replace( 
+		/[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+		function( m, key, value ) { // callback
+			VVvars[key] = value !== undefined ? value : ''
+		}
+	);
+
+	if ( fieldN ) {
+		return VVvars[fieldN] ? VVvars[fieldN] : null
+	}
+	return VVvars
 }
 
 function verif()
 {
+    
+
     var nb_ad=document.getElementById("nb_ad")
     console.log("Nb adultes"+nb_ad.value)
     var nb_children=document.getElementById("nb_children")
@@ -28,26 +74,29 @@ function verif()
     //console.log(end_d.value)
     var button_valider=document.getElementById("button_valider")
 
-    var price = document.getElementById('price');
+    var id_rbot=document.getElementById("id_rbot")
+    console.log("Id desti="+id)
+    console.log("ID_rbot="+id_rbot.value)
+    //var price = document.getElementById('price');
 
 
     
-    var today_d = new Date();
+    var today_d = new Date()
     console.log(today_d)
 
-    var start_dR = new Date(start_d.value);
-    var end_dR = new Date(end_d.value);
+    var start_dR = new Date(start_d.value)
+    var end_dR = new Date(end_d.value)
     //console.log(date_depart)
 
-    var gap = dayDiff(start_dR, end_dR);
+    var gap = gap_calcul(start_dR, end_dR)
     gap++
     console.log("gap entre date"+gap)
 
-    var gap_v= dayDiff(today_d, start_dR);
+    var gap_v= gap_calcul(today_d, start_dR)
     gap_v++
     console.log("Gap premiere date"+gap_v)
 
-    if((gap_v > 0 )&& (nb_ad.value >= 1) && (gap >= 1))
+    if((gap_v > 0 )&& (nb_ad.value >= 1) && (gap >= 1) && (id_rbot.value==id))
     {
         button_valider.disabled = false
     }
@@ -56,9 +105,13 @@ function verif()
         button_valider.disabled = true
     }
   
+
+    price_calculation(nb_ad, nb_children, breakfast, gap);
+    //console.log("prix calcule"+price_calulated)
+    //console.log(VVvars.getById(id).price)
 }
 
-function dayDiff(d1, d2)
+function gap_calcul(d1, d2)
 {
   d1 = d1.getTime() / 86400000;
   d2 = d2.getTime() / 86400000;
@@ -66,13 +119,13 @@ function dayDiff(d1, d2)
 }
 
 
-function price_calculation(nb_ad, nb_children, breakfast, start_d, end_d)
+function price_calculation(nb_ad, nb_children, breakfast, gap)
 {
 
-    if (!isNaN(var_json.getById(id).price * gap)) { //var_json données récupérées dans le json, voir déclaration début de fichier
+    if (!isNaN(VVvars.getById(id).price * gap)) { //VVvar données récupérées dans le json, voir déclaration début de fichier
         var price_calc = 0
-        price_calc = var_json.getById(id).price * gap * nb_ad.value;
-        price_calc = price_calc + var_json.getById(id).price * 0.40 * nb_children.value * gap
+        price_calc = VVvars.getById(id).price * gap * nb_ad.value;
+        price_calc = price_calc + VVvars.getById(id).price * 0.40 * nb_children.value * gap
         if (breakfast.checked)
         {
             price_calc = price_calc + (12 * (Number(nb_ad.value) + Number(nb_children.value)) * gap);
